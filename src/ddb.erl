06db3afdbccb44ba0826b262fb619806935e869c
Niclas Axelsson <niclas@burbas.se>
2013-06-18 10:36:38 +0200

    Add endpoint as an argument to credentials/4

--- src/ddb.erl
+++ src/ddb.erl
@@ -23,22 +23,22 @@
 
 -module(ddb).
 
--export([credentials/3, tables/0,
+-export([credentials/3, credentials/4, tables/0,
          key_type/2, key_type/4,
          key_value/2, key_value/4,
          create_table/4, describe_table/1, remove_table/1,
-         get/2, get/3, put/2, update/3, update/4, 
-         delete/2, delete/3, 
-	 cond_put/3,
+         get/2, get/3, put/2, update/3, update/4,
+         delete/2, delete/3,
+         cond_put/3,
          cond_update/4, cond_update/5,
          cond_delete/3, cond_delete/4,
          now/0, find/3, find/4,
-	 q/3, q/4,
-	 scan/2, scan/3,
-	 range_key_condition/1]).
+         q/3, q/4,
+         scan/2, scan/3,
+         range_key_condition/1]).
 
 -define(DDB_DOMAIN, "dynamodb.us-east-1.amazonaws.com").
--define(DDB_ENDPOINT, "https://" ++ ?DDB_DOMAIN ++ "/").
+-define(DDB_ENDPOINT, "https://" ++ get_ddb_domain() ++ "/").
 -define(DDB_AMZ_PREFIX, "x-amz-").
 
 -define(SIGNATURE_METHOD, "HmacSHA1").
@@ -65,7 +65,7 @@
 -define(TG_PUT_ITEM, ?TG_VERSION ++ "PutItem").
 -define(TG_GET_ITEM, ?TG_VERSION ++ "GetItem").
 -define(TG_UPDATE_ITEM, ?TG_VERSION ++ "UpdateItem").
--define(TG_DELETE_ITEM, ?TG_VERSION ++ "DeleteItem"). 
+-define(TG_DELETE_ITEM, ?TG_VERSION ++ "DeleteItem").
 -define(TG_QUERY, ?TG_VERSION ++ "Query").
 -define(TG_SCAN, ?TG_VERSION ++ "Scan").
 
@@ -88,13 +88,19 @@
 -type json_parameters() :: [json_parameter()].
 
 %%% Set temporary credentials, use ddb_iam:token/1 to fetch from AWS.
- 
+
 -spec credentials(string(), string(), string()) -> 'ok'.
 
 credentials(AccessKeyId, SecretAccessKey, SessionToken) ->
     'ok' = application:set_env('ddb', 'accesskeyid', AccessKeyId),
     'ok' = application:set_env('ddb', 'secretaccesskey', SecretAccessKey),
     'ok' = application:set_env('ddb', 'sessiontoken', SessionToken).
+
+-spec credentials(string(), string(), string(), string()) -> 'ok'.
+
+credentials(AccessKeyId, SecretAccessKey, SessionToken, DDBDomain) ->
+    'ok' = application:set_env('ddb', 'ddb_domain', DDBDomain),
+    credentials(AccessKeyId, SecretAccessKey, SessionToken).
 
 %%% Retrieve stored credentials.
 
@@ -113,29 +119,29 @@ credentials() ->
 key_type(HashKey, HashKeyType)
   when is_binary(HashKey),
        is_atom(HashKeyType) ->
-    [{<<"HashKeyElement">>, 
+    [{<<"HashKeyElement">>,
       [{<<"AttributeName">>, HashKey},
        {<<"AttributeType">>, type(HashKeyType)}]}].
 
 -spec key_type(binary(), type(), binary(), type()) -> json().
 
-key_type(HashKey, HashKeyType, RangeKey, RangeKeyType) 
+key_type(HashKey, HashKeyType, RangeKey, RangeKeyType)
   when is_binary(HashKey),
        is_atom(HashKeyType),
        is_binary(RangeKey),
        is_atom(RangeKeyType) ->
-    [{<<"HashKeyElement">>, 
+    [{<<"HashKeyElement">>,
       [{<<"AttributeName">>, HashKey},
        {<<"AttributeType">>, type(HashKeyType)}]},
-     {<<"RangeKeyElement">>, 
+     {<<"RangeKeyElement">>,
       [{<<"AttributeName">>, RangeKey},
        {<<"AttributeType">>, type(RangeKeyType)}]}].
 
 %%% Create table. Use key_type/2 or key_type/4 as key.
 
 -spec create_table(tablename(), key_json(), pos_integer(), pos_integer()) -> json_reply().
- 
-create_table(Name, Keys, ReadsPerSec, WritesPerSec) 
+
+create_table(Name, Keys, ReadsPerSec, WritesPerSec)
   when is_binary(Name),
        is_list(Keys),
        is_integer(ReadsPerSec),
@@ -159,7 +165,7 @@ tables() ->
 
 -spec describe_table(tablename()) -> json_reply().
 
-describe_table(Name) 
+describe_table(Name)
   when is_binary(Name) ->
     JSON = [{<<"TableName">>, Name}],
     request(?TG_DESCRIBE_TABLE, JSON).
@@ -168,12 +174,12 @@ describe_table(Name)
 
 -spec remove_table(tablename()) -> json_reply().
 
-remove_table(Name) 
+remove_table(Name)
   when is_binary(Name) ->
     JSON = [{<<"TableName">>, Name}],
     request(?TG_DELETE_TABLE, JSON).
 
-%%% Put item attributes into table. 
+%%% Put item attributes into table.
 
 -spec put(tablename(), [put_attr()]) -> json_reply().
 
@@ -192,7 +198,7 @@ cond_put(Name, Attributes, Condition)
        is_list(Attributes) ->
     JSON = [{<<"TableName">>, Name},
             {<<"Item">>, format_put_attrs(Attributes)}]
-	++ format_update_cond(Condition),
+        ++ format_update_cond(Condition),
     request(?TG_PUT_ITEM, JSON).
 
 %%% Create a key value, either hash or hash and range.
@@ -202,21 +208,21 @@ cond_put(Name, Attributes, Condition)
 key_value(HashKeyValue, HashKeyType)
   when is_binary(HashKeyValue),
        is_atom(HashKeyType) ->
-    [{<<"Key">>, [{<<"HashKeyElement">>, 
+    [{<<"Key">>, [{<<"HashKeyElement">>,
                    [{type(HashKeyType), HashKeyValue}]}]}].
 
 -spec key_value(binary(), type(), binary(), type()) -> json().
 
-key_value(HashKeyValue, HashKeyType, RangeKeyValue, RangeKeyType) 
+key_value(HashKeyValue, HashKeyType, RangeKeyValue, RangeKeyType)
   when is_binary(HashKeyValue),
        is_atom(HashKeyType),
        is_binary(RangeKeyValue),
        is_atom(RangeKeyType) ->
-    [{<<"Key">>, [{<<"HashKeyElement">>, 
+    [{<<"Key">>, [{<<"HashKeyElement">>,
                    [{type(HashKeyType), HashKeyValue}]},
-                  {<<"RangeKeyElement">>, 
+                  {<<"RangeKeyElement">>,
                    [{type(RangeKeyType), RangeKeyValue}]}]}].
-    
+
 %%% Update attributes of an existing item.
 
 -spec update(tablename(), key_json(), [update_attr()]) -> json_reply().
@@ -232,11 +238,11 @@ update(Name, Keys, Attributes, Returns)
        is_list(Attributes),
        is_atom(Returns) ->
     JSON = [{<<"TableName">>, Name},
-            {<<"ReturnValues">>, returns(Returns)}] 
-        ++ Keys 
+            {<<"ReturnValues">>, returns(Returns)}]
+        ++ Keys
         ++ [{<<"AttributeUpdates">>, format_update_attrs(Attributes)}],
     request(?TG_UPDATE_ITEM, JSON).
-    
+
 %%% Conditionally update attributes of an existing item.
 
 -spec cond_update(tablename(), key_json(), [update_attr()], update_cond()) -> json_reply().
@@ -252,11 +258,11 @@ cond_update(Name, Keys, Attributes, Condition, Returns)
        is_list(Attributes),
        is_atom(Returns) ->
     JSON = [{<<"TableName">>, Name},
-            {<<"ReturnValues">>, returns(Returns)}] 
-        ++ Keys 
+            {<<"ReturnValues">>, returns(Returns)}]
+        ++ Keys
         ++ [{<<"AttributeUpdates">>, format_update_attrs(Attributes)}]
         ++ format_update_cond(Condition),
-    request(?TG_UPDATE_ITEM, JSON).    
+    request(?TG_UPDATE_ITEM, JSON).
 
 %%% Delete existing item.
 
@@ -272,10 +278,10 @@ delete(Name, Keys, Returns)
        is_list(Keys),
        is_atom(Returns) ->
     JSON = [{<<"TableName">>, Name},
-            {<<"ReturnValues">>, returns(Returns)}] 
+            {<<"ReturnValues">>, returns(Returns)}]
         ++ Keys,
     request(?TG_DELETE_ITEM, JSON).
-    
+
 %%% Conditionally delete existing item.
 
 -spec cond_delete(tablename(), key_json(), update_cond()) -> json_reply().
@@ -290,11 +296,11 @@ cond_delete(Name, Keys, Condition, Returns)
        is_list(Keys),
        is_atom(Returns) ->
     JSON = [{<<"TableName">>, Name},
-            {<<"ReturnValues">>, returns(Returns)}] 
-        ++ Keys 
+            {<<"ReturnValues">>, returns(Returns)}]
+        ++ Keys
         ++ format_update_cond(Condition),
-    request(?TG_DELETE_ITEM, JSON).    
-    
+    request(?TG_DELETE_ITEM, JSON).
+
 %%% Fetch all item attributes from table.
 
 -spec get(tablename(), key_json()) -> json_reply().
@@ -312,9 +318,9 @@ get(Name, Keys)
 get(Name, Keys, Parameters)
   when is_binary(Name),
        is_list(Keys) ->
-    JSON = [{<<"TableName">>, Name}] 
-	++ Keys
-	++ Parameters,
+    JSON = [{<<"TableName">>, Name}]
+        ++ Keys
+        ++ Parameters,
     request(?TG_GET_ITEM, JSON).
 
 %%% Fetch all item attributes from table using a condition.
@@ -333,10 +339,10 @@ find(Name, {HashKeyValue, HashKeyType}, RangeKeyCond, StartKey)
        is_binary(HashKeyValue),
        is_atom(HashKeyType) ->
     JSON = [{<<"TableName">>, Name},
-            {<<"HashKeyValue">>, 
+            {<<"HashKeyValue">>,
              [{type(HashKeyType), HashKeyValue}]},
-	    range_key_condition(RangeKeyCond)]
-	++ start_key(StartKey),
+            range_key_condition(RangeKeyCond)]
+        ++ start_key(StartKey),
 
     request(?TG_QUERY, JSON).
 
@@ -348,15 +354,15 @@ range_key_condition({Condition, RangeKeyType, RangeKeyValues})
        is_atom(RangeKeyType),
        is_list(RangeKeyValues) ->
     {Op, Values} = case Condition of
-                       'between' -> 
+                       'between' ->
                            [A, B] = RangeKeyValues,
-                           {<<"BETWEEN">>, [[{type(RangeKeyType), A}], 
+                           {<<"BETWEEN">>, [[{type(RangeKeyType), A}],
                                             [{type(RangeKeyType), B}]]};
                        'equal' ->
                            {<<"EQ">>, [[{type(RangeKeyType), hd(RangeKeyValues)}]]}
                    end,
     {<<"RangeKeyCondition">>, [{<<"AttributeValueList">>, Values},
-			       {<<"ComparisonOperator">>, Op}]}.
+                               {<<"ComparisonOperator">>, Op}]}.
 
 %%% Query a table
 
@@ -376,8 +382,8 @@ q(Name, {HashKeyValue, HashKeyType}, Parameters, StartKey)
        is_list(Parameters) ->
     JSON = [{<<"TableName">>, Name},
             {<<"HashKeyValue">>, [{type(HashKeyType), HashKeyValue}]}]
-	++ Parameters
-	++ start_key(StartKey),
+        ++ Parameters
+        ++ start_key(StartKey),
     request(?TG_QUERY, JSON).
 
 %%% Scan a table
@@ -395,18 +401,28 @@ scan(Name, Parameters, StartKey)
   when is_binary(Name),
        is_list(Parameters) ->
     JSON = [{<<"TableName">>, Name}]
-	++ Parameters
-	++ start_key(StartKey),
+        ++ Parameters
+        ++ start_key(StartKey),
     request(?TG_SCAN, JSON).
 
 %%%
 %%% Helper functions
 %%%
 
+-spec get_ddb_domain() -> string().
+
+get_ddb_domain() ->
+    case application:get_env('ddb', 'ddb_domain') of
+        {'ok', DDBDomain} ->
+            DDBDomain;
+        _ ->
+            ?DDB_DOMAIN
+    end.
+
 -spec start_key(json() | 'none') -> json_parameters().
-start_key('none') -> 
+start_key('none') ->
     [];
-start_key(StartKey) -> 
+start_key(StartKey) ->
     [{<<"ExclusiveStartKey">>, StartKey}].
 
 -spec format_put_attrs([put_attr()]) -> json().
@@ -425,15 +441,15 @@ format_update_attrs(Attributes) ->
                  ({Name, 'delete'}) ->
                       {Name, [{<<"Action">>, update_action('delete')}]}
               end, Attributes).
-    
+
 -spec format_update_cond(update_cond()) -> json().
 
-format_update_cond({'does_not_exist', Name}) -> 
+format_update_cond({'does_not_exist', Name}) ->
     [{<<"Expected">>, [{Name, [{<<"Exists">>, <<"false">>}]}]}];
 
-format_update_cond({'exists', Name, Value, Type}) -> 
+format_update_cond({'exists', Name, Value, Type}) ->
     [{<<"Expected">>, [{Name, [{<<"Value">>, [{type(Type), Value}]}]}]}].
-     
+
 -spec type(type()) -> binary().
 
 type('string') -> <<"S">>;
@@ -454,7 +470,7 @@ returns('updated_new') -> <<"UPDATED_NEW">>.
 update_action('put') -> <<"PUT">>;
 update_action('add') -> <<"ADD">>;
 update_action('delete') -> <<"DELETE">>.
-     
+
 -spec request(string(), json()) -> json_reply().
 
 request(Target, JSON) ->
@@ -464,12 +480,12 @@ request(Target, JSON) ->
     Opts = [{'response_format', 'binary'}],
     F = fun() -> ibrowse:send_req(?DDB_ENDPOINT, [{'Content-type', ?CONTENT_TYPE} | Headers], 'post', Body, Opts) end,
     case ddb_aws:retry(F, ?MAX_RETRIES, fun jsx:json_to_term/1) of
-	{'error', 'expired_token'} ->
-	    {ok, Key, Secret, Token} = ddb_iam:token(129600),
-	    ddb:credentials(Key, Secret, Token),
-	    request(Target, JSON);
-	Else ->
-	    Else
+        {'error', 'expired_token'} ->
+            {ok, Key, Secret, Token} = ddb_iam:token(129600),
+            ddb:credentials(Key, Secret, Token),
+            request(Target, JSON);
+        Else ->
+            Else
     end.
 
 -spec headers(string(), binary()) -> proplists:proplist().
@@ -488,7 +504,7 @@ headers(Target, Body) ->
 
 authorization(AccessKeyId, SecretAccessKey, Headers, Body) ->
     Signature = signature(SecretAccessKey, Headers, Body),
-    lists:flatten(io_lib:format("AWS3 AWSAccessKeyId=~s,Algorithm=~s,Signature=~s", 
+    lists:flatten(io_lib:format("AWS3 AWSAccessKeyId=~s,Algorithm=~s,Signature=~s",
                                 [AccessKeyId, ?SIGNATURE_METHOD, Signature])).
 
 -spec signature(string(), proplists:proplist(), binary()) -> string().
@@ -503,7 +519,7 @@ signature(SecretAccessKey, Headers, Body) ->
 canonical(Headers) ->
     Headers1 = lists:map(fun({K, V}) -> {ddb_util:to_lower(K), V} end, Headers),
     Amz = lists:filter(fun({K, _V}) -> lists:prefix(?DDB_AMZ_PREFIX, K) end, Headers1),
-    Headers2 = [{ddb_util:to_lower(?HOST_HEADER), ?DDB_DOMAIN}|lists:sort(Amz)],
+    Headers2 = [{ddb_util:to_lower(?HOST_HEADER), get_ddb_domain()}|lists:sort(Amz)],
     lists:map(fun({K, V}) -> [K, ":", V, "\n"] end, Headers2).
 
 -spec now() -> pos_integer().
