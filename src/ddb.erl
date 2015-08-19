9ad99006e452e6a883f8a0115beef3015f35d511
danikp <dmitry.polyanovsky@gmail.com>
2015-08-19 20:04:31 +0300

    update ibrowse to latest fix few warnings

--- src/ddb.erl
+++ src/ddb.erl
@@ -511,8 +511,8 @@ authorization(AccessKeyId, SecretAccessKey, Headers, Body) ->
 
 signature(SecretAccessKey, Headers, Body) ->
     StringToSign = lists:flatten(["POST", $\n, "/", $\n, $\n, canonical(Headers), $\n, Body]),
-    BytesToSign = crypto:sha(StringToSign),
-    base64:encode_to_string(binary_to_list(crypto:sha_mac(SecretAccessKey, BytesToSign))).
+    BytesToSign = crypto:hash(sha, StringToSign),
+    base64:encode_to_string(binary_to_list(crypto:hmac(sha, SecretAccessKey, BytesToSign))).
 
 -spec canonical(proplists:proplist()) -> [_].
 
